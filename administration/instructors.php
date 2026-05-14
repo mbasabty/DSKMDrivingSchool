817ecd88522f68fd4a99c5181fdd5b4858354488
Mbasa <mbasabatyi@icloud.com>
2026-05-14 21:53:10 +0200

    remove some folders added instructors menu added admin menu used coookies for user rights

---
+++ administration/instructors.php
@@ -0,0 +1,44 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['logged']) || $_SESSION['logged'] !== true) {
+    die("Access denied. Please login first.");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Instructors Menu</title>
+    <link rel="stylesheet" href="../incl/style/administration/instructors.css">
+</head>
+
+<body>
+
+    <h2>Instructors Menu</h2>
+
+    <!-- SHOW LOGGED IN USER -->
+     <p class="welcome">
+        <b>Welcome <?= htmlspecialchars($_SESSION['name']) ?> !, what would you like to do?</b>
+    </p>
+
+    <!-- ONLY BUTTONS INSIDE -->
+    <div class="page-container">
+
+        <div class="button-group">
+
+            <a href="#">
+                View Assigned Drivers
+            </a>
+
+            <a href="#">
+                View History
+            </a>
+        </div>
+
+    </div>
+
+</body>
+</html>
