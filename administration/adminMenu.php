90b349b34e5b207a7ea625fd7ed7c94b8ee8152e
Mbasa <mbasabatyi@icloud.com>
2026-05-17 22:05:54 +0200

    added background and changed some color text for: adminMenu instructors login index

--- administration/adminMenu.php
+++ administration/adminMenu.php
@@ -1,49 +1,38 @@
 <?php
-session_start();
+    session_start();
 
-if (!isset($_SESSION['logged']) || $_SESSION['logged'] !== true) {
-    die("Access denied. Please login first.");
-}
+    if (!isset($_SESSION['logged']) || $_SESSION['logged'] !== true) {
+        die("Access denied. Please login first.");
+    }
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Administration Menu</title>
+        <link rel="stylesheet" href="../incl/style/administration/adminMenu.css">
+    </head>
 
-<head>
-    <meta charset="UTF-8">
-    <title>Administration Menu</title>
-    <link rel="stylesheet" href="../incl/style/administration/adminMenu.css">
-</head>
-
-<body>
-
-    <h2>Menu</h2>
-
-    <!-- SHOW LOGGED IN USER -->
-     <p class="welcome">
-        <b>Welcome <?= htmlspecialchars($_SESSION['name']) ?> !, what would you like to do?</b>
-    </p>
-
-    <!-- ONLY BUTTONS INSIDE -->
-    <div class="page-container">
-
-        <div class="button-group">
-
-            <a href="#">
-                Sales Dashboard
-            </a>
-
-            <a href="#">
-                Assigning Instructors
-            </a>
-
-            <a href="#">
-                View Staff Details
-            </a>
-
+    <body>
+        <h2>Menu</h2>
+        <!-- SHOW LOGGED IN USER -->
+        <p class="welcome">
+            <b>Welcome <?= htmlspecialchars($_SESSION['name']) ?> !, what would you like to do?</b>
+        </p>
+        <!-- ONLY BUTTONS INSIDE -->
+        <div class="page-container">
+            <div class="button-group">
+                <a href="#">
+                    SALES DASHBOARD
+                </a>
+                <a href="#">
+                    VIEW STAFF DETAILS
+                </a>
+                <a href="#">
+                    ASSIGNING INSTRUCTORS TO DRIVERS
+                </a>
+            </div>
         </div>
-
-    </div>
-
-</body>
+    </body>
 </html>
